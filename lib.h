a77982fbdf3a18710e5a4d8655f22bc8fd3335b7
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-04-05 20:03:16 -0300

    Librerias

---
+++ lib.h
@@ -0,0 +1,7 @@
+#ifndef LIB_H_INCLUDED
+#define LIB_H_INCLUDED
+
+int getInt(char *mensaje, char *error, int intentos, int maximo, int minimo, int *resultado);
+float getFloat(char *mensaje, char *error, int intentos, float maximo, float minimo, float *resultado);
+int mostrarArrayInt(int* array, int cantidad);
+#endif // LIB_H_INCLUDED
